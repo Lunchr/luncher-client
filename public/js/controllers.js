05205037b502295eeaff6f3266be0c10f4e92490
Deiwin <deiwin.sarjas@cgi.com>
2014-03-30 20:02:17 +0300

    merge with latest index2
    delete index2 to avoid confusion

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,15 +2,18 @@ var phonecatApp = angular.module('phonecatApp', []);
  
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
   $scope.offers = [
-    {'location': 'Asian Chef',
+    {'id': '1',
+     'location': 'Asian Chef',
      'title': 'Sweet & Sour Chicken',
      'description': 'Kanafilee aedviljadega rikkalikus magushapus kastmes.',
      'price': 3.4},
-    {'location': 'Asian Chef',
+    {'id': '2',
+     'location': 'Asian Chef',
      'title': 'Sweet & Sour Beef',
      'description': 'Seafilee aedviljadega rikkalikus magushapus kastmes.',
      'price': 3.5},
-    {'location': 'Asian Chef',
+    {'id': '3',
+     'location': 'Asian Chef',
      'title': 'Sweet & Sour Duck',
      'description': 'Pardifilee aedviljadega rikkalikus magushapus kastmes.',
      'price': 3.6} 
