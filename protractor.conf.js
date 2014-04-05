24c9552d71783f161bbc607801ccdef02e7e62ef
Deiwin <deiwin.sarjas@cgi.com>
2014-04-06 01:47:47 +0300

    fix syntax errors in protractor conf
    also delete npm install -g bower form the travis conf - it's already habndled by grunt

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,9 +3,9 @@ exports.config = {
   specs: ['test/e2e/*Spec.js']
 }
 if (process.env.TRAVIS){
-  exports.config.sauceUser: process.env.SAUCE_USERNAME,
-  exports.config.sauceKey: process.env.SAUCE_ACCESS_KEY,
-  exports.config.capabilities: {
+  exports.config.sauceUser = process.env.SAUCE_USERNAME,
+  exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY,
+  exports.config.capabilities = {
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
     'build': process.env.TRAVIS_BUILD_NUMBER
   }
