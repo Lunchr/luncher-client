9d986a7c548856a56c839eff5503d59acd07e89a
Deiwin <deiwin.sarjas@cgi.com>
2014-04-06 10:34:03 +0300

    add tokens via the sauce connect addon
    try not using the env variables

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,12 +3,7 @@ exports.config = {
   specs: ['test/e2e/*Spec.js']
 };
 if (process.env.TRAVIS){
-  var sauceUser = process.env.SAUCE_USERNAME;
-  var sauceKey = process.env.SAUCE_ACCESS_KEY;
-
-  exports.config.sauceUser = sauceUser;
-  exports.config.sauceKey = sauceKey;
-  exports.config.seleniumAddress = 'http://' + sauceUser + ':' + sauceKey + '@localhost:4445/wd/hub';
+  exports.config.seleniumAddress = 'http://localhost:4445/wd/hub';
   exports.config.capabilities = {
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
