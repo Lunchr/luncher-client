461c70569f5ce81a2291879496650a0f2664eef2
Deiwin <deiwin.sarjas@cgi.com>
2014-04-06 10:51:24 +0300

    try using accessKey instead of access-key
    as descriebed here: http://support.saucelabs.com/entries/23001310-Common-Error-Messages-Automated-Testing-

--- protractor.conf.js
+++ protractor.conf.js
@@ -6,7 +6,7 @@ if (process.env.TRAVIS){
   exports.config.seleniumAddress = 'http://localhost:4445/wd/hub';
   exports.config.capabilities = {
     'username': process.env.SAUCE_USERNAME,
-    'access-key': process.env.SAUCE_ACCESS_KEY,
+    'accessKey': process.env.SAUCE_ACCESS_KEY,
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
   };
