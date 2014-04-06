e5959733dbb4078b4cad81b76b9acf45e2b1759b
Deiwin <deiwin.sarjas@cgi.com>
2014-04-06 10:46:45 +0300

    add saucelab tokens to capabilities
    readd saucelab env variable

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,6 +5,8 @@ exports.config = {
 if (process.env.TRAVIS){
   exports.config.seleniumAddress = 'http://localhost:4445/wd/hub';
   exports.config.capabilities = {
+    'username': process.env.SAUCE_USERNAME,
+    'access-key': process.env.SAUCE_ACCESS_KEY,
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
   };
