ac223450d3e5360c5a8b279b9337abc28476d3f9
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-04-11 19:17:19 +0300

    add not text to toHaveClass matcher

--- test/utils.js
+++ test/utils.js
@@ -6,8 +6,10 @@ var utils = (function (){
   beforeEach(function() {
     this.addMatchers({
       toHaveClass: function(cls) {
+        var actualDump = angular.mock.dump(this.actual);
+        var notText = this.isNot ? ' not' : '';
         this.message = function() {
-          return "Expected '" + angular.mock.dump(this.actual) + "' to have class '" + cls + "'.";
+          return 'Expected "' + actualDump + '" '+ notText + ' to have class "' + cls + '".';
         };
 
         return this.actual.hasClass(cls);
