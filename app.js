a9e0269fec4e85c54706d3913beb1fb9aeb95a2d
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-04-27 17:08:01 +0300

    use express to statically serve content
    create dummy test to test mocha and coverage reporting

---
+++ app.js
@@ -0,0 +1,26 @@
+
+/**
+ * Module dependencies
+ */
+
+var express = require('express'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+
+/**
+ * Configuration
+ */
+
+app.set('port', process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ test/server/appSpec.js
@@ -0,0 +1,7 @@
+describe('app', function() {
+  'use strict';
+
+  it('should have should.js available', function() {
+    true.should.be.ok;
+  });
+});
