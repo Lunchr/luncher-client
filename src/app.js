a902b96ee42a6f6a2732148566fb7cf411cc32a8
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-05-03 14:09:59 +0300

    add mongo connection

--- src/app.js
+++ src/app.js
@@ -6,9 +6,26 @@
 var express = require('express'),
   http = require('http'),
   path = require('path'),
-  api = require('./routes/api');
+  api = require('./routes/api'),
+  mongoose = require('mongoose');
 
 var app = module.exports = express();
+
+var connect = function () {
+  var options = { server: { socketOptions: { keepAlive: 1 } } }
+  mongoose.connect('mongodb://localhost/test', options)
+}
+connect()
+
+// Error handler
+mongoose.connection.on('error', function (err) {
+  console.error(err);
+})
+
+// Reconnect when closed
+mongoose.connection.on('disconnected', function () {
+  connect()
+})
 
 
 /**
