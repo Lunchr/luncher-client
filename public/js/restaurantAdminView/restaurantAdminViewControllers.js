6155054a8a85673cbf69ed477e26e3c450a95370
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-03-15 19:39:52 +0200

    update the offer form to handle POST request failures

--- public/js/restaurantAdminView/restaurantAdminViewControllers.js
+++ public/js/restaurantAdminView/restaurantAdminViewControllers.js
@@ -16,6 +16,18 @@
       $scope.offers = $resource('api/v1/restaurants/me/offers').query();
       $scope.$on(offerPostedEventChannel, function(event, offer) {
         $scope.offers.unshift(offer);
+
+        offer.confirmationPending = true;
+        offer.$promise.then(function(){
+          offer.confirmationPending = false;
+        }, function(){
+          // we could, in theory, use shift(), but I don't think we can guarantee at
+          // this point that this offer is still the first one
+          var index = $scope.offers.indexOf(offer);
+          if (index > -1) {
+            $scope.offers.splice(index, 1);
+          }
+        });
       });
     }
   ]);
