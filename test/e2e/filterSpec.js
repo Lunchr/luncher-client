10c3a799e796fb81b1608a946876cc75af803c40
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-04-11 23:06:25 +0300

    fix e2e tests

--- test/e2e/filterSpec.js
+++ test/e2e/filterSpec.js
@@ -2,24 +2,30 @@ describe('Praad App', function() {
   'use strict';
 
   describe('Offer list view', function() {
-    var offers;
+    var offers, clickPromise;
 
     beforeEach(function() {
       browser.get('/');
       offers = element.all(by.repeater('offer in offers'));
+      clickPromise = element(by.model('userWantsProximal')).click().then(function() {
+        return element(by.css('option[label=Tartu]')).click();
+      });
     });
 
     it('should initially have 3 offers', function() {
-      offers.count(); // I don't know why but this fixes an issue with the count being 0
-      expect(offers.count()).toBe(13);
+      clickPromise.then(function() {
+        expect(offers.count()).toBe(13);
+      });
     });
 
     it('should filter the offer list as user types into the search box', function() {
-      element(by.model('query')).sendKeys('kana');
+      clickPromise.then(function() {
+        element(by.model('query')).sendKeys('kana');
 
-      expect(offers.count()).toBe(1);
-      var title = offers.first().element(by.binding('offer.title')).getText();
-      expect(title).toBe('Pekingi kana');
+        expect(offers.count()).toBe(1);
+        var title = offers.first().element(by.binding('offer.title')).getText();
+        expect(title).toBe('Pekingi kana');
+      });
     });
   });
 });
