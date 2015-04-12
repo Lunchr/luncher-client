d0c9ee3407bf4fc801b0ba2737077a87029d0ea7
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-04-12 20:39:35 +0300

    update offer source selection to remember the last selected region

--- public/src/mainView/offerList/sourceSelection/offerSourceSelection.directive.js
+++ public/src/mainView/offerList/sourceSelection/offerSourceSelection.directive.js
@@ -3,10 +3,11 @@
   var module = angular.module('sourceSelection', [
     'regionSelection',
     'geolocator',
+    'cookies',
   ]);
 
-  module.directive('offerSourceSelection', ['$window',
-    function($window) {
+  module.directive('offerSourceSelection', ['$window', 'cookies',
+    function($window, cookies) {
       return {
         scope: {
           onRegionSelected: '&',
@@ -17,16 +18,30 @@
             return !$window.navigator.geolocation;
           };
           $scope.regionSelected = function(region) {
+            cookies.setOfferSource({
+              region: region,
+            });
             $scope.onRegionSelected({
               $region: region,
             });
           };
           $scope.locationSelected = function(lat, lng) {
+            cookies.setOfferSource({
+              location: true,
+            });
             $scope.onLocationSelected({
               $lat: lat,
               $lng: lng,
             });
           };
+
+          var offerSource = cookies.getOfferSource();
+          if (!offerSource) {
+            return;
+          }
+          if (offerSource.region){
+            $scope.regionSelected(offerSource.region);
+          }
         },
         restrict: 'E',
         templateUrl: 'src/mainView/offerList/sourceSelection/offerSourceSelection.template.html',
--- test/client/mainView/offerList/sourceSelection/offerSourceSelection.directiveSpec.js
+++ test/client/mainView/offerList/sourceSelection/offerSourceSelection.directiveSpec.js
@@ -1,43 +1,88 @@
 describe('RegionSelection', function() {
   'use strict';
+  var cookies;
   beforeEach(function() {
     module('sourceSelection', 'partials', function($provide){
       $provide.factory('regionSelectionDirective', function(){ return {}; });
+      $provide.factory('geolocator', function(){ return {}; });
+      $provide.provider('cookies', {
+        $get: function() {
+          return {
+            refreshExpirations: function(){},
+            setOfferSource: jasmine.createSpy('setOfferSource'),
+            getOfferSource: jasmine.createSpy('getOfferSource'),
+            removeOfferSource: jasmine.createSpy('removeOfferSource'),
+          };
+        }
+      });
+    });
+    inject(function(_cookies_) {
+      cookies = _cookies_;
     });
   });
 
   describe('offer source selection directive', function() {
     var element, $scope, $parentScope;
 
-    beforeEach(inject(function($httpBackend) {
-      var compiled = utils.compile('<offer-source-selection on-region-selected="regionSelected($region)"'+
-      'on-location-selected="locationSelected($lat, $lng)"></offer-source-selection>');
-      element = compiled.element;
-      $scope = compiled.scope;
-      $parentScope = compiled.parentScope;
-    }));
-
-    describe('onRegionSelected', function() {
+    describe('with offer source cookie set for a region', function() {
       beforeEach(function() {
-        $parentScope.regionSelected = jasmine.createSpy('regionSelected');
+        var compiled = utils.compile('<offer-source-selection on-region-selected="regionSelected($region)"></offer-source-selection>',
+          function(parentScope) {
+            cookies.getOfferSource.and.returnValue({region: 'a-region'});
+            parentScope.regionSelected = jasmine.createSpy('regionSelected');
+          }
+        );
+        element = compiled.element;
+        $scope = compiled.scope;
+        $parentScope = compiled.parentScope;
       });
 
-      it('should call the specified function with $region as the argument when option selected from dropdown', function() {
-        $scope.regionSelected('test');
-
-        expect($parentScope.regionSelected).toHaveBeenCalledWith('test');
+      it('should have called region selected on parent on createion', function() {
+        expect($parentScope.regionSelected).toHaveBeenCalledWith('a-region');
       });
     });
 
-    describe('onLocationSelected', function() {
+    describe('with no cookie set', function() {
       beforeEach(function() {
-        $parentScope.locationSelected = jasmine.createSpy('locationSelected');
+        var compiled = utils.compile('<offer-source-selection on-region-selected="regionSelected($region)"'+
+        'on-location-selected="locationSelected($lat, $lng)"></offer-source-selection>');
+        element = compiled.element;
+        $scope = compiled.scope;
+        $parentScope = compiled.parentScope;
       });
 
-      it('should call the specified function with $region as the argument when option selected from dropdown', function() {
-        $scope.locationSelected('lat', 'lng');
+      describe('onRegionSelected', function() {
+        beforeEach(function() {
+          $parentScope.regionSelected = jasmine.createSpy('regionSelected');
+        });
 
-        expect($parentScope.locationSelected).toHaveBeenCalledWith('lat', 'lng');
+        it('should call the specified function with $region as the argument when option selected from dropdown', function() {
+          $scope.regionSelected('test');
+
+          expect($parentScope.regionSelected).toHaveBeenCalledWith('test');
+        });
+
+        it('should set the offerSource cookie to the selected region', function() {
+          $scope.regionSelected('test');
+          expect(cookies.setOfferSource).toHaveBeenCalledWith({region: 'test'});
+        });
+      });
+
+      describe('onLocationSelected', function() {
+        beforeEach(function() {
+          $parentScope.locationSelected = jasmine.createSpy('locationSelected');
+        });
+
+        it('should call the specified function with $region as the argument when option selected from dropdown', function() {
+          $scope.locationSelected('lat', 'lng');
+
+          expect($parentScope.locationSelected).toHaveBeenCalledWith('lat', 'lng');
+        });
+
+        it('should set the offerSource cookie to location', function() {
+          $scope.locationSelected('lat', 'lng');
+          expect(cookies.setOfferSource).toHaveBeenCalledWith({location:true});
+        });
       });
     });
   });
