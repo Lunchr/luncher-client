79c6c8f5faceafde2f18adabd7920221eb91d32f
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-04-17 18:42:59 +0300

    update geolocator to work even if multiple directives are loaded at once
    Previously multiple directive instances loaded the Google Maps API for
    for every instance. Also, the only one of the instances got notified of the
    user location, others were left pending.
    
    Also, timeout was added, so that if the user doesn't approve the usage of
    their location within 10 seconds, an error message can be displayed.

--- public/src/mainView/offerList/sourceSelection/geolocator/geolocator.directive.js
+++ public/src/mainView/offerList/sourceSelection/geolocator/geolocator.directive.js
@@ -2,24 +2,42 @@
   'use strict';
   var module = angular.module('geolocator', []);
 
-  module.factory('locatorMap', ['$window', '$q',
-    function($window, $q) {
-      var map, locator;
-      function loadMapScript(canvasID, key) {
-        var script = document.createElement('script');
-        var mapsDefer = $q.defer();
-        script.type = 'text/javascript';
-        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&callback=googleMapsInitialized';
-        if (key) {
-          script.src += '&key=' + key;
+  function Locator(marker, readyPromise) {
+    this.marker = marker;
+    this.readyPromise = readyPromise;
+  }
+  Locator.prototype.getLocation = function() {
+    var location = this.marker.getPosition();
+    return {
+      lat: location.lat(),
+      lng: location.lng(),
+    };
+  }
+
+  module.factory('locatorMap', ['$window', '$q', '$timeout',
+    function($window, $q, $timeout) {
+      var mapsAPIPromise, geolocationPromise;
+      function loadMap(canvasID, key) {
+        if (!mapsAPIPromise) {
+          var mapsDefer = $q.defer();
+          mapsAPIPromise = mapsDefer.promise;
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&callback=googleMapsInitialized';
+          if (key) {
+            script.src += '&key=' + key;
+          }
+          $window.googleMapsInitialized = mapsDefer.resolve;
+          $window.document.body.appendChild(script);
         }
-        $window.googleMapsInitialized = mapsDefer.resolve;
-        $window.document.body.appendChild(script);
-        mapsDefer.promise.then(function(){
-          initMap(canvasID);
+        return mapsAPIPromise.then(function(){
+          var map = initMap(canvasID);
+          var locatorMarker = initLocatorMarker(map);
+          var readyPromise = centerOnUsersLocation(map, locatorMarker);
+          return new Locator(locatorMarker, readyPromise);
         });
       }
-      function handleNoGeolocation(errorFlag) {
+      function handleNoGeolocation(map, errorFlag) {
         var content;
         if (errorFlag) {
           content = 'Error: The Geolocation service failed.';
@@ -36,12 +54,23 @@
         var infowindow = new $window.google.maps.InfoWindow(options);
         map.setCenter(options.position);
       }
-      function centerOnUsersLocation() {
+      function centerOnUsersLocation(map, locatorMarker) {
         if(!$window.navigator.geolocation) {
-          handleNoGeolocation(false);
-          return;
+          handleNoGeolocation(map, false);
+          return $q.reject('Geolocation service not supported.');
         }
-        $window.navigator.geolocation.getCurrentPosition(function(position) {
+        if (!geolocationPromise) {
+          var geolocationDefer = $q.defer();
+          geolocationPromise = geolocationDefer.promise;
+          $window.navigator.geolocation.getCurrentPosition(geolocationDefer.resolve, geolocationDefer.reject);
+          var timeoutPromise = $timeout(function(){
+            geolocationDefer.reject('Timed out');
+          }, 10000);
+          geolocationPromise.then(function() {
+            $timeout.cancel(timeoutPromise);
+          });
+        }
+        return geolocationPromise.then(function(position) {
           var pos = new $window.google.maps.LatLng(position.coords.latitude, position.coords.longitude);
           var marker = new $window.google.maps.Marker({
             clickable: false,
@@ -72,34 +101,36 @@
             center: pos,
             map: map,
           });
-          locator = new $window.google.maps.Marker({
-            draggable: true,
-            zIndex: 3,
-            position: pos,
-            map: map,
-          });
+          locatorMarker.setPosition(pos);
 
           map.setCenter(pos);
         }, function() {
-          handleNoGeolocation(true);
+          handleNoGeolocation(map, true);
+          return $q.reject('Geolocation service failed.');
         });
       }
       function initMap(canvasID) {
         var mapOptions = {
           zoom: 17,
         };
-        map = new $window.google.maps.Map($window.document.getElementById(canvasID), mapOptions);
-        centerOnUsersLocation();
+        return new $window.google.maps.Map($window.document.getElementById(canvasID), mapOptions);
+      }
+      function initLocatorMarker(map) {
+        return new $window.google.maps.Marker({
+          draggable: true,
+          zIndex: 3,
+          map: map,
+        });
       }
       return {
-        loadMapScript: loadMapScript,
-        getLocation: function() {
-          var location = locator.getPosition();
-          return {
-            lat: location.lat(),
-            lng: location.lng(),
-          };
-        },
+        /**
+         * loadMap will initialize google maps, if it isn't already initialized and
+         * will then initialize a map on the specified canvasID. Once the user has
+         * accepted to share their location the map will be centered to that location
+         * and a marker will be displayed that the user can move to confirm/specify
+         * their actual location.
+         */
+        loadMap: loadMap,
       };
     }
   ]);
@@ -113,18 +144,26 @@
           ngShow: '=',
         },
         link: function($scope, $element, $attrs) {
+          var locator;
           $scope.locationSelected = function() {
-            var location = locatorMap.getLocation();
+            var location = locator.getLocation();
             $scope.onLocationSelected({
               $lat: location.lat,
               $lng: location.lng,
             });
           };
+          $scope.ready = false;
           var deregister = $scope.$watch('ngShow', function(newVal, oldVal) {
             if (!newVal) {
               return;
             }
-            locatorMap.loadMapScript('geolocator-canvas-'+$scope.$id, $scope.key);
+            var locatorPromise = locatorMap.loadMap('geolocator-canvas-'+$scope.$id, $scope.key);
+            locatorPromise.then(function(_locator_) {
+              locator = _locator_;
+              locator.readyPromise.then(function() {
+                $scope.ready = true;
+              });
+            });
             // only init the map the first time this directive is made visible
             deregister();
           });
