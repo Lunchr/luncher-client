0778418d79655209932c08af182d47ae54669c69
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-05-24 19:49:16 +0300

    implement submit action for registration pages step

--- public/src/register/pages.controller.js
+++ public/src/register/pages.controller.js
@@ -2,10 +2,13 @@
   'use strict';
   var module = angular.module('registerPages', []);
 
-  module.controller('RegisterPagesCtrl', ['pages',
-    function(pages) {
+  module.controller('RegisterPagesCtrl', ['$location', 'pages',
+    function($location, pages) {
       var vm = this;
       vm.pages = pages;
+      vm.submit = function() {
+        $location.path('/register/form/' + vm.selectedPage);
+      };
     }
   ]);
 })();
