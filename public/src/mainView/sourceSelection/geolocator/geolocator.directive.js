39ba027dce2d7306c6df7b3c9f3824b7cd6b87e8
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-05-31 12:33:47 +0300

    update explanatory comment for $timeout call
    some formatting as well

--- public/src/mainView/sourceSelection/geolocator/geolocator.directive.js
+++ public/src/mainView/sourceSelection/geolocator/geolocator.directive.js
@@ -18,8 +18,8 @@
         opacity: 0,
       });
       ngGeolocatorProvider.extendStaticCircleOptions({
-          fillOpacity: 0.1,
-          strokeOpacity: 0,
+        fillOpacity: 0.1,
+        strokeOpacity: 0,
       });
     }
   ]);
@@ -54,20 +54,18 @@
             if (!newVal) {
               return;
             }
-            // The small timeout is used as a hacky fix to an issue where, if the Google Maps API
-            // is already loaded, the initialization is almost instant and finishes before the
-            // ngShow animation does. This means that the map will be initialized for a zero sized
-            // canvas which messes things up. The timeout hack avoids this. I tried looking for a
-            // way to listen to when the animation finishes, but didn't find anything good. Initializing
-            // this with a callback after the animation would be a much nicer fix, though.
+            // $timeout forces the function to be called after the modified DOM
+            // has been rendered by the browser and as we are just making the canvas
+            // visible, we want to wait until it has actually been rendered otherwise
+            // we get an error from the Google Maps API.
             $timeout(function() {
-              ngGeolocator.create('geolocator-canvas-'+$scope.$id).then(function success(locator) {
+              ngGeolocator.create('geolocator-canvas-' + $scope.$id).then(function success(locator) {
                 ctrl.locator = locator;
                 ctrl.ready = true;
               }, function failure(message) {
                 ctrl.error = true;
               });
-            }, 10);
+            });
             // only init the map the first time this directive is made visible
             deregister();
           });
