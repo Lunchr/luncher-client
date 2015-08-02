0f26172dc7e5ef8b22d32e65158e98f2bbbbed8d
Deiwin Sarjas <deiwin@salemove.com>
2015-08-02 13:14:06 +0300

    Update registration form submit enabling error labels
    If the submit button is clicked before the form is valid, we now enable
    the input field specific error messages. This helps draw the user's
    attention to the data they still have to input/fix.

--- public/src/register/form.controller.js
+++ public/src/register/form.controller.js
@@ -37,7 +37,9 @@
       vm.isReadyForError = function() {
         for (var i = 0; i < arguments.length; i++) {
           var input = arguments[i];
-          if (input.$dirty && input.$touched && input.$invalid)
+          // If the user has clicked submit, we want to draw their attention to the
+          // invalid fields.
+          if ((vm.submitClicked || (input.$dirty && input.$touched)) && input.$invalid)
             return true;
         }
         return false;
