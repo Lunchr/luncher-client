fc21e224dfddf0b5993d8bd085b0c4f71723609e
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-08-20 17:31:13 +0300

    Fix prefilled date field texts
    The date wasn't formatted. The current solution is still a bit
    problematic, though. The prefilled date's don't move the date selector
    to the correct month. It will always initially show the current month.

--- public/src/restaurantAdminView/offerForm/offerFormDirective.js
+++ public/src/restaurantAdminView/offerForm/offerFormDirective.js
@@ -15,8 +15,8 @@
     }
   ]);
 
-  module.directive('offerForm', ['$resource', 'filterFilter',
-    function($resource, filterFilter) {
+  module.directive('offerForm', ['$resource', 'filterFilter', 'dateFilter',
+    function($resource, filterFilter, dateFilter) {
       return {
         scope: {
           offerToEdit: '=edit',
@@ -43,8 +43,7 @@
               $scope.ingredients = offer.ingredients;
               $scope.tags = offer.tags;
               $scope.price = offer.price;
-              $scope.date = new Date(offer.from_time);
-              $scope.date.setHours(0, 0, 0, 0);
+              $scope.date = dateFilter(new Date(offer.from_time), 'justDate');
               $scope.fromTime = new Date(offer.from_time);
               $scope.fromTime.setFullYear(1970);
               $scope.fromTime.setMonth(0);
