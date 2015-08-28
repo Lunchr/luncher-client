3a863fee240d52e2f963605fafac13b1dcf49a8e
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-08-28 06:32:03 +0300

    Add a test case for when the group post GET 404s

--- test/client/restaurantAdminView/facebookGroupPost/facebookGroupPostDirectiveSpec.js
+++ test/client/restaurantAdminView/facebookGroupPost/facebookGroupPostDirectiveSpec.js
@@ -6,39 +6,53 @@ describe('facebookGroupPostDirective', function() {
 
   describe('facebook-group-post directive', function() {
     var ctrl, element, $scope, $parentScope;
-    var postResponse = memo().is(function() {
-      return {
-        _id: 1234,
-        date: '2011-04-04',
-        message_template: 'a message template',
-      };
-    });
+    var postResponse = memo().is(function() {});
+
+	beforeEach(inject(function($httpBackend) {
+	  $httpBackend.expectGET('api/v1/restaurant/posts/2011-04-04').respond(postResponse());
+	  var compiled = utils.compile('<facebook-group-post ' +
+		'on-submit="submitClicked()" ' +
+		'on-cancel="cancelClicked()" ' +
+		'default-template="defaultTemplate" ' +
+		'date="date" ' +
+		'</facebook-group-post>',
+	  function(parentScope) {
+		parentScope.date = "2011-04-04";
+		parentScope.defaultTemplate = "default message template";
+	  });
+	  element = compiled.element;
+	  $scope = compiled.scope;
+	  ctrl = $scope.ctrl;
+	  $parentScope = compiled.parentScope;
+	}));
 
 	context('with a successful initial GET', function() {
-	  beforeEach(inject(function($httpBackend) {
-		$httpBackend.expectGET('api/v1/restaurant/posts/2011-04-04').respond(postResponse());
-		var compiled = utils.compile('<facebook-group-post ' +
-		  'on-submit="submitClicked()" ' +
-		  'on-cancel="cancelClicked()" ' +
-		  'default-template="defaultTemplate" ' +
-		  'date="date" ' +
-		  '</facebook-group-post>',
-		function(parentScope) {
-		  parentScope.date = "2011-04-04";
-		  parentScope.defaultTemplate = "default template message";
-		});
-		element = compiled.element;
-		$scope = compiled.scope;
-		ctrl = $scope.ctrl;
-		$parentScope = compiled.parentScope;
-	  }));
+	  postResponse.is(function() {
+		return {
+		  _id: 1234,
+		  date: '2011-04-14',
+		  message_template: 'a message template',
+		};
+	  });
 
-	  it('should have tags data after we mock-respond to the HTTP request', inject(function($httpBackend) {
+	  it('sets the post info onto the controller', inject(function($httpBackend) {
 		expect(ctrl.post.$resolved).toBe(false);
 		$httpBackend.flush();
 		expect(ctrl.post._id).toEqual(1234);
-		expect(ctrl.post.date).toEqual('2011-04-04');
+		expect(ctrl.post.date).toEqual('2011-04-14');
 		expect(ctrl.post.message_template).toEqual('a message template');
+	  }));
+	});
+
+	context('with initial GET returning 404', function() {
+	  postResponse.is(function() { return 404; });
+
+	  it('loads the default template for the post', inject(function($httpBackend) {
+		expect(ctrl.post.$resolved).toBe(false);
+		$httpBackend.flush();
+		expect(ctrl.post._id).not.toBeDefined();
+		expect(ctrl.post.date).toEqual('2011-04-04');
+		expect(ctrl.post.message_template).toEqual('default message template');
 	  }));
 	});
   });
