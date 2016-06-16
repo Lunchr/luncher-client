51dac9d2a5cf080871fe6641a283f6f338900b10
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2016-06-16 21:57:57 +0300

    Fix e2e tests after default region change
    The tests expected the region selection popup to appear, but right now
    we default to the Tallinn region instead, in which case, to select the
    Tartu region, the test must select that region from the dropdown.

--- test/e2e/filterSpec.js
+++ test/e2e/filterSpec.js
@@ -2,23 +2,28 @@ describe('Praad App', function() {
   'use strict';
 
   describe('Offer list view', function() {
-    var offers, clickPromise;
+    var offers, regionSelectedPromise;
 
     beforeEach(function() {
       browser.manage().deleteAllCookies();
       browser.get('/');
-      clickPromise = element(by.css('.popup')).element(by.css('label')).click();
+      regionSelectedPromise = element(by.css('.icon-22')).click().then(function() {
+        var EC = protractor.ExpectedConditions;
+        var tartuButton = element(by.cssContainingText('.dropdown label', 'Tartu'));
+        browser.wait(EC.elementToBeClickable(tartuButton), 500);
+        return tartuButton.click();
+      });
       offers = element.all(by.repeater('offer in restaurant.offers'));
     });
 
     it('should initially have 3 offers', function() {
-      clickPromise.then(function() {
+      regionSelectedPromise.then(function() {
         expect(offers.count()).toBe(37);
       });
     });
 
     it('should filter the offer list as user types into the search box', function() {
-      clickPromise.then(function() {
+      regionSelectedPromise.then(function() {
         element(by.model('search.query')).sendKeys('kana');
 
         expect(offers.count()).toBe(7);
